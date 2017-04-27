fb3ad403f6fedd870b131fdcf03ba39795a1ac5d
okulneva <olga.kulneva@dataart.com>
2017-04-27 20:36:08 +0300

    init

---
+++ LambdaCompilation.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    using System.CodeDom.Compiler;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    using Microsoft.CSharp;
+
+    public static class LambdaCompilation
+    {
+
+        private static readonly string NamespaceName = "LambdaExp";
+        private static readonly string ClassName = "LambdaClass";
+
+        //public static object CompileLambda(string lambdaExpression)
+        //{
+        //    string source = LambdaClassWrapper(
+        //       lambdaExpression);
+        //    CompilerResults results = CompileAssemblyFromSource(source);
+        //    return GetExpression<object>(results);
+        //}
+        public static Action<T> CompileLambda<T>(string lambdaExpression)
+        {
+            string source = LambdaClassWrapper(
+                "Action<" + typeof(T)+ ">",
+                lambdaExpression);
+
+            CompilerResults results = CompileAssemblyFromSource(source);
+            return GetExpression<Action<T>>(results);
+            
+        }
+
+        public static Func<T, TResult> CompileLambda<T, TResult>(string lambdaExpression)
+        {
+            string source = LambdaClassWrapper("Func<" + typeof(T) + ", " + typeof(TResult) + ">", lambdaExpression);
+
+            CompilerResults results = CompileAssemblyFromSource(source);
+            return GetExpression<Func<T, TResult>>(results);
+        }
+
+        private static CompilerResults CompileAssemblyFromSource(string source)
+
+        {
+            CompilerParameters parameters = new CompilerParameters();
+
+            Assembly oCurrentAssembly = typeof(Program).Assembly;
+            foreach (AssemblyName oName in oCurrentAssembly.GetReferencedAssemblies())
+            {
+                var strLocation = Assembly.ReflectionOnlyLoad(oName.FullName).Location;
+                parameters.ReferencedAssemblies.Add(strLocation);
+            }
+            string exeName = Assembly.GetEntryAssembly().Location;
+            parameters.ReferencedAssemblies.Add(exeName);
+
+            parameters.GenerateExecutable = false;
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            ICodeCompiler icc = codeProvider.CreateCompiler();
+
+
+            CompilerResults results = icc.CompileAssemblyFromSource(parameters, source);
+            return results;
+        }
+
+        private static T GetExpression<T>(CompilerResults results)
+            where T: class
+        {
+            if (!results.Errors.HasErrors && !results.Errors.HasWarnings)
+            {
+                Assembly ass = results.CompiledAssembly;
+
+                object lambdaClassInstance =
+                        ass.CreateInstance(NamespaceName + "." + ClassName);
+
+                object lambdaMethodResult =
+                       lambdaClassInstance.GetType().GetProperty("Prop").GetValue(lambdaClassInstance);
+
+                return lambdaMethodResult as T;
+            }
+            return null;
+        }
+
+        private static string LambdaClassWrapper(string type,string exp)
+        {
+            string source =
+                    "using System;" +
+                    "namespace " + NamespaceName +
+                    "{" +
+                        "public class " + ClassName +
+                        "{" +
+                            type + " field = "+ exp+"; " +
+                            "public "+ type+" Prop {get { return field; }}" +
+                        "}" +
+                    "}";
+            return source;
+        }
+
+        private static string LambdaClassWrapper(string exp)
+        {
+            string source =
+                    "using System;  using System.Linq.Expressions;" +
+                    "namespace " + NamespaceName +
+                    "{" +
+                        "public class " + ClassName +
+                        "{" +
+                            "Expression fieldExp = " + exp + "; " +
+                            "public Expression Prop {get { return fieldExp; }}" +
+                        "}" +
+                    "}";
+            return source;
+        }
+    }
+}
